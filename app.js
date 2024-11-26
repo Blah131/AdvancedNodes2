44ce50de97506db1132227bed06f722d6a28acfe
Kane <kbukowski@ivytech.edu>
2024-11-26 17:57:07 -0600

    Finished Video 9

--- app.js
+++ app.js
@@ -23,57 +23,29 @@ app.use(express.static('public'));
 app.use(morgan('dev'));
 
 
-// mongoose and mongo sandbox routes
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'new blog 2',
-        snippet: 'about my new blog',
-        body: 'more about my new blog'
-    });
+//
 
-    blog.save()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
 
-app.get('/all-blogs', (req,res) => {
-    Blog.find()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch ((err) => {
-            console.log(err);
-        });
-});
-
-app.get('/single-blog', (req,res) => {
-    Blog.findById('67465b88c410278b08bff51f')
-    .then((result) => {
-        res.send(result)
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-})
 
 //routes
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi fidns eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    ];
-    //res.send('<p>homepage</p>');
-    res.render('index', { title: 'Home', blogs });
+    res.redirect('/blogs')
 });
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' })
 });
+
+// blog routes
+app.get('/blogs', (req,res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render('index', { title: 'All Blogs', blogs: result })
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
 
 app.get('/blogs/create', (req,res) => {
     res.render('create', { title: 'Create a new Blog' });
